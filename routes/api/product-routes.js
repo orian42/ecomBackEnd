aa5e55199d2506febdf8de1f6b1f103aadb8a0de
Jason Ryan <164967582+orian42@users.noreply.github.com>
2024-07-11 14:18:17 -0700

    Product queries complete

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -111,8 +111,20 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const delProduct = await Product.destroy({
+        where: {
+          id: req.params.id,
+        }
+      });
+    console.log('Product successfully deleted!');
+    res.status(200).json(delProduct);
+  } catch (error) {
+    console.error('Error deleting product:', error);
+    res.status(500).json({ error: 'Failed to delete product' });
+  }
 });
 
 module.exports = router;
